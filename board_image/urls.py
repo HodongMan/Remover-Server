3f73e670939ba244b98655aff97ae7198b14e7a7
hodongman <arshavin3@naver.com>
2018-03-14 21:17:27 +0900

    image shuffle update

--- board_image/urls.py
+++ board_image/urls.py
@@ -3,9 +3,11 @@ from django.urls import path
 from .views import (
     BoardImageList,
     BoardImageDetail,
+    BoardImageRandomList,
 )
 
 urlpatterns = [
     path('', BoardImageList.as_view(), name = BoardImageList.name),
     path('<int:pk>/', BoardImageDetail.as_view(), name = BoardImageDetail.name),
+    path('random/', BoardImageRandomList.as_view(), name = BoardImageRandomList.name),
 ]
--- board_image/views.py
+++ board_image/views.py
@@ -1,3 +1,4 @@
+from random import shuffle, sample
 from rest_framework import generics
 
 from .models import BoardImage
@@ -15,3 +16,15 @@ class BoardImageDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = BoardImage.objects.all()
     serializer_class = BoardImageSerializer
     name = 'board-image-detail'
+
+class BoardImageRandomList(generics.ListAPIView):
+
+    queryset = BoardImage.objects.all()
+    serializer_class = BoardImageSerializer
+    name = 'board-image-random-list'
+
+    def get_queryset(self):
+
+        board_image_list = list(BoardImage.objects.all())
+        shuffle(board_image_list)
+        return board_image_list
