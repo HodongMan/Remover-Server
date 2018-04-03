a52dbe488d5ce3eb07fdac70f9146908c0993f26
hodongman <arshavin3@naver.com>
2018-04-04 06:46:25 +0900

    profile default and image updae

--- user/models.py
+++ user/models.py
@@ -5,6 +5,6 @@ class User(models.Model):
 
     user = models.CharField(max_length=200, primary_key=True)
     name = models.CharField(max_length=200)
-    profile_image = models.CharField(max_length=200)
+    profile_image = models.CharField(max_length=200, default="http://13.125.76.112/statics/profile/1.png")
 
     
